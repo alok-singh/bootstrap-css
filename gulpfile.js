d819d2b30b3d58f74a99e9f3c8ab1d3ad97e1b40
Admin <admin@Admins-MacBook-Pro.local>
2021-06-06 21:19:32 +0800

    progress

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const cleanCSS = require('gulp-clean-css');
+
+gulp.task('minify-css', () => {
+  return gulp
+    .src(['custom.css', 'libs/css/*.css'])
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('dist/libs/css'));
+});
+
+gulp.task('copy-libs', function() {
+  return gulp.src('./libs/js/*.js')
+    .pipe(gulp.dest('dist/libs/js'));
+});
+
+gulp.task('copy-images', function() {
+  return gulp.src('./img/*')
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('minify-html', () => {
+  return gulp
+    .src('index.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
+});
